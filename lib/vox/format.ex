087f4860136bf7997f7147d80e79fff33b080a29
Stefan Johnson <sj@scrimpycat.io>
2020-08-18 20:08:03 +1000

    Document format

--- lib/vox/format.ex
+++ lib/vox/format.ex
@@ -1,5 +1,22 @@
 defmodule Vox.Format do
+    @moduledoc """
+      Behaviour for representing different voxel formats.
+    """
+
+    @doc """
+      Implement the behaviour creating interfaceable voxel data from an
+      underlying voxel format.
+
+      Return the interfaceable voxel data.
+    """
     @callback new(any) :: Vox.Data.t
 
+    @doc """
+      Implement the behaviour for determining whether the data is in the
+      format by this behaviour.
+
+      If the data is in the format of this behaviour then return `true`,
+      otherwise return `false`.
+    """
     @callback format?(any) :: boolean
 end
