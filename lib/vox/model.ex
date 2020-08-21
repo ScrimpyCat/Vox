b8a3355071a0eb4cc23c3295472aec1a1460b516
Stefan Johnson <sj@scrimpycat.io>
2020-08-21 22:25:44 +1000

    Document model

--- lib/vox/model.ex
+++ lib/vox/model.ex
@@ -1,4 +1,7 @@
 defmodule Vox.Model do
+    @moduledoc """
+      A voxel model.
+    """
     @type bounds_error :: :bounds
     @type unknown_error :: :unknown
     @type error(type) :: { :error, { :model, type } }
@@ -11,19 +14,34 @@ defmodule Vox.Model do
 
     defstruct [size: { 0, 0, 0 }, voxels: %{}]
 
+    @doc """
+      Get the width of the model.
+    """
     @spec width(t) :: axis
     def width(%{ size: { w, _, _ } }), do: w
 
+    @doc """
+      Get the height of the model.
+    """
     @spec width(t) :: axis
     def height(%{ size: { _, h, _ } }), do: h
 
+    @doc """
+      Get the depth of the model.
+    """
     @spec width(t) :: axis
     def depth(%{ size: { _, _, d } }), do: d
 
+    @doc """
+      Get a voxel in the model.
+    """
     @spec voxel(t, point) :: { :ok, Vox.Voxel.t | nil } | error(bounds_error)
     def voxel(%{ size: { w, h, d } }, { x, y, z }) when (x < 0) or (x >= w) or (y < 0) or (y >= h) or (z < 0) or (z >= d), do: { :error, { :model, :bounds } }
     def voxel(%{ voxels: voxels }, point), do: { :ok, voxels[point] }
 
+    @doc """
+      Get a voxel in the model.
+    """
     @spec voxel(t, axis, axis, axis) :: { :ok, Vox.Voxel.t | nil } | error(bounds_error)
     def voxel(model, x, y, z), do: voxel(model, { x, y, z })
 
@@ -42,6 +60,9 @@ defmodule Vox.Model do
         def message(%{ model: %{ size: size }, point: point }), do: "(#{inspect point}) outside of model's bounds: #{inspect size}"
     end
 
+    @doc """
+      Get a voxel in the model.
+    """
     @spec voxel!(t, point) :: Vox.Voxel.t | nil | no_return
     def voxel!(model, point) do
         case voxel(model, point) do
@@ -50,6 +71,9 @@ defmodule Vox.Model do
         end
     end
 
+    @doc """
+      Get a voxel in the model.
+    """
     @spec voxel!(t, axis, axis, axis) :: Vox.Voxel.t | nil | no_return
     def voxel!(model, x, y, z), do: voxel!(model, { x, y, z })
 end
